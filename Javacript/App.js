7d9afdbb5d0fbcdcda95e613042e3408734a7858
Urtzi Arana Santamaria <me@MacBook-Air-de-Urtzi.local>
2024-10-10 00:15:35 +0200

    changes

--- Javacript/App.js
+++ Javacript/App.js
@@ -16,8 +16,28 @@ function choosePoker(option) {
     } else {
         window.location.href = "https://aaronrs2002.github.io/poker/?" + gaParam + "&theme=" + setTheme + "&balance=" + balance + "&";
     }
-
-
-
-
 }
+
+function openGame(gameId) {
+    // Map the gameId to the actual game page URLs
+    var gamePages = {
+      'game1': '../BattleChess/Chess.html',
+      'game2': 'Poker.html',
+      'game3': '../BattleRisk/Main.html'
+    };
+  
+    // Get the URL for the selected game
+    var gameUrl = gamePages[gameId];
+  
+    if (gameUrl) {
+      // Check if the game page exists (optional)
+      // This requires an additional request, which can be complex due to cross-origin policies
+      // For simplicity, we'll proceed to navigate
+  
+      // Navigate to the game page
+      window.location.href = gameUrl;
+    } else {
+      // Display an alert to the user
+      alert('Sorry, the selected game is not available.');
+    }
+  }
